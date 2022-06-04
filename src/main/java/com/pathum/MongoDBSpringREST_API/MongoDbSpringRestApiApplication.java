f1e8a2bccbea5b8a5fa0084352ffff9fbb3653ae
Pathum Liyanagama <pathumpml.dev@gmail.com>
2022-06-04 16:37:36 +0530

    Initial commit

---
+++ src/main/java/com/pathum/MongoDBSpringREST_API/MongoDbSpringRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.pathum.MongoDBSpringREST_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbSpringRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoDbSpringRestApiApplication.class, args);
+	}
+
+}
